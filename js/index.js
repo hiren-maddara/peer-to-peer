825986974d08f3c892899a23070089e76f90a458
hiren-maddara <hiren.maddara@gmail.com>
2023-02-21 23:08:57 +0300

    First commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// const btns = document.querySelectorAll('.btn')
+// const forms = document.querySelectorAll('[class*="form"]')
+
+// const btnApply = document.querySelector('.apply__btn')
+// const chkbxAgree = document.querySelector('#agree')
+
+// // if(chkbx.checked) proceed
+// btnApply.disabled = true;
+// chkbxAgree.addEventListener('click', () => {
+//     if (chkbxAgree.checked) {
+//         btnApply.disabled = false
+//     }
+// })
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', (e) => e.preventDefault())
+// })
+
+// console.log(forms)
+
+// forms.forEach(form => {
+//     form.addEventListener('submit', (e) => e.preventDefault())
+// })
