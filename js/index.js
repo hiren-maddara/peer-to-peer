6ce07ecc3f656675d0793d39923fa163447cc182
Hiren Maddara <hiren.maddara@gmail.com>
2023-03-24 17:03:05 +0300

    'remake'

--- js/index.js
+++ js/index.js
@@ -1,43 +1,18 @@
-// const btns = document.querySelectorAll('.btn')
-// const forms = document.querySelectorAll('[class*="form"]')
+document.addEventListener("DOMContentLoaded", (e) => {
+  const btnMenuToggle = document.querySelector(".nav__menu--btn");
+  const navMenu = document.querySelector(".nav__list");
 
-// const btnApply = document.querySelector('.apply__btn')
-// const chkbxAgree = document.querySelector('#agree')
+  window.addEventListener("load", function (e) {
+    if (e.target.innerWidth < 760) navMenu.classList.add("--collapse");
+    if (e.target.innerWidth > 760) navMenu.classList.remove("--collapse");
+  });
 
-// // if(chkbx.checked) proceed
-// btnApply.disabled = true;
-// chkbxAgree.addEventListener('click', () => {
-//     if (chkbxAgree.checked) {
-//         btnApply.disabled = false
-//     }
-// })
+  btnMenuToggle.addEventListener("click", (e) => {
+    navMenu.classList.toggle("--collapse");
+  });
 
-// btns.forEach(btn => {
-//     btn.addEventListener('click', (e) => e.preventDefault())
-// })
-
-// console.log(forms)
-
-// forms.forEach(form => {
-//     form.addEventListener('submit', (e) => e.preventDefault())
-// })
-
-const btnMenuToggle = document.querySelector('.nav__menu--btn')
-const navMenu = document.querySelector('.nav__list')
-
-navMenu.classList.add('--collapse')
-
-window.addEventListener('load', function(e){
-    if (e.target.innerWidth < 760) navMenu.classList.add('--collapse')  
-    if (e.target.innerWidth > 760) navMenu.classList.remove('--collapse')
-})
-
-btnMenuToggle.addEventListener('click', e => {
-    navMenu.classList.toggle('--collapse')    
-})
-
-window.addEventListener('resize', e => {
-    if (e.target.innerWidth < 760) navMenu.classList.add('--collapse')  
-    if (e.target.innerWidth > 760) navMenu.classList.remove('--collapse')
-})
-
+  window.addEventListener("resize", (e) => {
+    if (e.target.innerWidth < 760) navMenu.classList.add("--collapse");
+    if (e.target.innerWidth > 760) navMenu.classList.remove("--collapse");
+  });
+});
